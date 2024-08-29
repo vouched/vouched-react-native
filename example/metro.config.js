3c6cb709cd5c7926fb8b802cfb4968ef44830768
Miguel Machado <mmachado53@gmail.com>
2024-08-29 16:24:44 -0700

    fix: update example Android to use gradle 8.7, compileSDK 34

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,36 +1,11 @@
-// metro.config.js
-//
-// with multiple workarounds for this issue with symlinks:
-// https://github.com/facebook/metro/issues/1
-//
-// with thanks to @johnryan (<https://github.com/johnryan>)
-// for the pointers to multiple workaround solutions here:
-// https://github.com/facebook/metro/issues/1#issuecomment-541642857
-//
-// see also this discussion:
-// https://github.com/brodybits/create-react-native-module/issues/232
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-const path = require("path");
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
 
-module.exports = {
-  // workaround for an issue with symlinks encountered starting with
-  // metro@0.55 / React Native 0.61
-  // (not needed with React Native 0.60 / metro@0.54)
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      { get: (_, name) => path.resolve(".", "node_modules", name) }
-    ),
-  },
-
-  // quick workaround for another issue with symlinks
-  watchFolders: [".", ".."],
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
